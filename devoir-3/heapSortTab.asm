6de0a59bbf531d33a32265f2d22ed1a0692847d5
Etienne Collin <collin.etienne.contact@gmail.com>
2023-04-09 16:10:02 -0400

    Renamed file

--- devoir-3/heapSortTab.asm
+++ devoir-3/heapSortTab.asm
@@ -1,32 +1,47 @@
 # --------------------
 # Description:
-# Heap and heapSort implemented in MIPS assembly language
+# Heap and heapSort implemented in MIPS assembly.
 # --------------------
-# Authors :
+# Authors:
 # Etienne Collin 20237904
 # Ange Lilian Tchomtchoua Tokam 20230129
 # Justin Villeneuve 20132792
+# --------------------
+# Contact:
+# etienne.collin@umontreal.ca
 # --------------------
 
 .data
 arrayStart:			.word	0x10040000							# Address of first element of array
 n:					.word	0									# n = size of array (initializes at 0)
-arraySizeMsg:		.asciiz	"Enter the length of the array : "
-arrayInputMsg:		.asciiz	"Enter an element in the array : "
-sortingMsg:			.asciiz	"Sorting array...\n"
-arrayOpenMsg:		.asciiz	"Array: ["
+arraySizeMsg:		.asciiz	"Enter the length of the array: "
+arrayInputMsg:		.asciiz	"Enter an element in the array: "
+sortingMsg:			.asciiz	"Sorting array..."
+arrayOpenMsg:		.asciiz	"\nArray: ["
 arrayCloseMsg:		.asciiz	"]\n"
 arraySeparatorMsg:	.asciiz ", "
 
 .text
 .globl	main
 
+# --------------------
+# Main method of the program.
+# --------------------
+# Parameters:
+# No parameters used
+# --------------------
+# Registers used:
+# No registers used
+# --------------------
+# Returns:
+# No return
+# --------------------
 main:
-	jal	init
-	jal	printArray
-	jal	heapSort
-	jal	printArray
-	jal	exit
+	jal	init		# Initialize array
+	jal	printArray 	# Print array
+	jal	heapSort	# Sort array
+	jal	printArray	# Print array
+	jal	exit		# Exit program
 
 # --------------------
 # Initializes an array of n elements from address arrayStart
@@ -35,7 +50,7 @@ main:
 # Parameters:
 # No parameters used
 # --------------------
-# Used variables:
+# Registers used:
 # s0: Array address
 # s1: Address of current element in array
 # s2: Size of the array - 1
@@ -44,7 +59,14 @@ main:
 # Returns:
 # No return
 # --------------------
-init:	
+init:
+	# Store registers getting overridden
+	addi	$sp,	$sp,	-16				# Allocate 4 words
+	sw		$s0,	12($sp)					# Store $s0
+	sw		$s1,	8($sp)					# Store $s1
+	sw		$s2,	4($sp)					# Store $s2
+	sw		$s3,	0($sp)					# Store $s3
+
 	# Ask the user the length of the array
 	li		$v0,	4						# Print a message to the user
 	la		$a0,	arraySizeMsg			# Address of the message
@@ -57,20 +79,20 @@ init:
 	sw		$v0,	0($t0)					# Store the array size in n
 	move	$s2,	$v0						# Store array size in $s2
 	addi	$s2,	$s2,	-1				# Store last index of array (n - 1)
-	
+
 	# Initialize table elements
-	la		$s0,	arrayStart				# Load address of first element of array
-	lw		$s0,	0($s0)
+	la		$s0,	arrayStart				# Load label of arrayStart
+	lw		$s0,	0($s0)					# Load address of arrayStart
 	li		$s3,	0						# Counter initialized to 0
 
 # Fill the array
 initLoop:
 	bgt		$s3,	$s2,		initExit	# Verify if counter reached the end of the array
-	
+
 	# Get address of current element in array
 	sll		$t0,	$s3,	2				# Get offset on element i in array (i * 4)
 	add		$s1,	$s0,	$t0				# Get address of element
-	
+
 	# Ask user of input
 	li		$v0,	4						# Print a string
 	la		$a0,	arrayInputMsg			# Address of the message to print
@@ -79,13 +101,21 @@ initLoop:
 	# Get user input
 	li		$v0,	5						# Read integer
 	syscall
-	
+
 	sw		$v0,	0($s1)					# Store the integer in the array
 	addi	$s3,	$s3,		1			# Increment counter
 	j		initLoop
 
 # Jump back to main
 initExit:
+	# Reload overriden registers
+	lw		$s0,	12($sp)					# Reload $s0
+	lw		$s1,	8($sp)					# Reload $s1
+	lw		$s2,	4($sp)					# Reload $s2
+	lw		$s3,	0($sp)					# Reload $s3
+
+	addi	$sp,	$sp,	12				# Free allocated space
+
 	jr		$ra								# Return
 
 # --------------------
@@ -94,9 +124,9 @@ initExit:
 # Parameters:
 # No parameters used
 # --------------------
-# Variables used:
+# Registers used:
 # $s0: Index of last element in array
-# $s1: heapSortBuildHeap counter, initialized at i = (n - 1)/2 
+# $s1: heapSortBuildHeap counter, initialized at i = (n - 1)/2
 # --------------------
 # Returns:
 # No return
@@ -108,12 +138,12 @@ heapSort:
 	# Store registers getting overridden
 	sw		$s0,	4($sp)					# Store $s0
 	sw		$s1,	0($sp)					# Store $s1
-	
+
 	# Print status message telling user the array is being sorted
 	li		$v0,	4						# Print a status message telling user the array is being sorted
 	la		$a0,	sortingMsg				# Address of the message
 	syscall
-	
+
 	# Initialize $s0 = n - 1
 	la		$s0,	n						# Load address of n which will contain the array size
 	lw		$s0,	0($s0)					# Loads the value of n
@@ -125,13 +155,13 @@ heapSort:
 
 heapSortBuildHeap:
 	# Check condition
-	bltz	$s1,	heapSortSort				# If i < 0, start sorting
+	bltz	$s1,	heapSortSort			# If i < 0, start sorting
 
 	# Fix heap
 	move	$a0,	$s1						# $a0 = index of last element in array
 	move	$a1,	$s0						# $a1 = value of heapSortBuildHeap counter
 	jal	 fixHeap							# Call fixHeap($a0, $a1)
-	
+
 	# Decrement i
 	addi	$s1,	$s1,	-1				# Decrement i
 	j		heapSortBuildHeap
@@ -162,10 +192,10 @@ heapSortExit:
 	# Reload overriden registers
 	lw		$s0,	4($sp)					# Reload $s0
 	lw		$s1,	0($sp)					# Reload $s1
-	
+
 	addi	$sp,	$sp,	12				# Free allocated space
 	jr		$ra								# Return
-	
+
 
 # --------------------
 # Prints an array of n elements from address arrayStart.
@@ -173,7 +203,7 @@ heapSortExit:
 # Parameters:
 # No parameters used
 # --------------------
-# Variables used:
+# Registers used:
 # $s0: Address of the array
 # $s1: Last index of array
 # $s2: Index in array (Counter)
@@ -190,34 +220,34 @@ printArray:
 
 	# Prepare for loop
 	# Load address of array
-	la		$s0,	arrayStart
-	lw		$s0,	0($s0)
-	
+	la		$s0,	arrayStart				# Load label of arrayStart
+	lw		$s0,	0($s0)					# Load address of arrayStart
+
 	# Initialize counter i = 0
 	li		$s1,	0
-	
+
 	# Get array length
 	la		$s2,	n						# Load address of n which will contain the array size
 	lw		$s2,	0($s2)					# Loads the value of n
-	
+
 	# Print array opening bracket
 	li		$v0,	4						# Print an opening bracket at then end of the printed array
 	la		$a0,	arrayOpenMsg			# Address of the message
 	syscall
-	
+
 printArrayLoop:
 	bge		$s1,	$s2,	printArrayExit	# Check if we have gone through the entire array
-	
+
 	# Get the integer in the array and print it
 	sll		$t0,	$s1,	2				# i = i * 4
 	add		$t0,	$s0,	$t0				# Get address 4i(arrayStart) to get value at index i
 	lw		$a0,	0($t0)					# Get value in array
 	li		$v0,	1						# Print integer
 	syscall
-	
+
 	# Increment counter
-	addi	$s1,	$s1,	1				# i = i + 1	
-	
+	addi	$s1,	$s1,	1				# i = i + 1
+
 	# Check if a separator is needed
 	bge		$s1,	$s2,	printArrayLoop	# If we are at last element, do not print separator
 
@@ -234,18 +264,18 @@ printArrayExit:
 	li		$v0,	4						# Print a closing bracket at then end of the printed array
 	la		$a0,	arrayCloseMsg			# Address of the message
 	syscall
-	
+
 	# Reload overriden registers
 
 
 	lw		$s0,	8($sp)					# Reload $s0
 	lw		$s1,	4($sp)					# Reload $s1
 	lw		$s2,	0($sp)					# Reload $s2
-	
+
 	addi	$sp,	$sp,	12				# Free allocated space
 
 	jr		$ra								# Return
-	
+
 # --------------------
 # Terminates the program.
 # --------------------
@@ -291,32 +321,32 @@ fixHeap:
 	sw		$s3,	8($sp)						# Store $s3
 	sw		$s4,	4($sp)						# Store $s4
 	sw		$s5,	0($sp)						# Store $s5
-		
+
 	# Load array start
-	la		$s0,	arrayStart
-	lw		$s0,	0($s0)
-	
+	la		$s0,	arrayStart					# Load label of arrayStart
+	lw		$s0,	0($s0)						# Load address of arrayStart
+
 	# Get value of root
 	sll		$t0,	$a0,	2					# i = rootIndex * 4
 	add		$t0,	$s0,	$t0					# Get address of index i in array
 	lw		$s1,	0($t0)						# Load rootValue into $s1
 
-fixHeapWhile:	
+fixHeapWhile:
 	# Get left child index
 	jal		getLeftChildIndex
 	move	$s2,	$v0							# Store left child index into $s2
 
 	# Check if left child index is within bounds
 	bgt		$s2,	$a1,	fixHeapWhileExit	# If left child index > lastIndex, exit while loop
-	
+
 	# Get left child index
 	jal		getRightChildIndex
 	move	$s3,	$v0
 
-firstIf:
+fixHeapFirstIf:
 	# First comparison
-	bgt		$s3,	$a1,	secondIf			# If right child index <= lastIndex, compare
-	
+	bgt		$s3,	$a1,	fixHeapSecondIf		# If right child index <= lastIndex, compare
+
 	# Prepare second comparison
 	# Get value of left child
 	sll		$t0,	$s2,	2					# i = rootIndex * 4
@@ -326,38 +356,38 @@ firstIf:
 	sll		$t0,	$s3,	2					# i = rootIndex * 4
 	add		$t0,	$s0,	$t0					# Get address of index i in array
 	lw		$s5,	0($t0)						# Load left child into $s5
-	
+
 	# Second comparison
-	bge		$s4,	$s5,	secondIf			# If left child > right child
+	bge		$s4,	$s5,	fixHeapSecondIf		# If left child > right child
 	move	$s2,	$s3
-		
-secondIf:
+
+fixHeapSecondIf:
 	# Get value of left child
 	sll		$t0,	$s2,	2					# i = rootIndex * 4
 	add		$t0,	$s0,	$t0					# Get address of index i in array
 	lw		$s4,	0($t0)						# Load left child into $s4
-	
+
 	# Check condition
 	ble		$s4,	$s1,	fixHeapWhileExit	# if left child > rootValue
-	
+
 	# Update array
 	sll		$t0,	$a0,	2					# i = rootIndex * 4
 	add		$t0,	$s0,	$t0					# Get address of index i in array
 	sw		$s4,	0($t0)						# array[rootIndex] = array[leftChildIndex]
-	
+
 	# Update indices
 	move	$a0,	$s2							# rootIndex = leftChildIndex
-	
+
 	# Continue loop
 	j		fixHeapWhile
-	
+
 fixHeapWhileExit:
 	# Update array
 	sll		$t0,	$a0,	2					# i = rootIndex * 4
 	add		$t0,	$s0,	$t0					# Get address of index i in array
 	sw		$s1,	0($t0)						# array[rootIndex] = rootValue;
-	
-    
+
+
 fixHeapExit:
 	# Reload return address
 	lw		$ra,	24($sp)						# Reload return address
@@ -368,10 +398,10 @@ fixHeapExit:
 	lw		$s3,	8($sp)						# Reload $s3
 	lw		$s4,	4($sp)						# Reload $s4
 	lw		$s5,	0($sp)						# Reload $s5
-	
+
 	addi	$sp,	$sp,	28					# Free allocated space
 	jr		$ra									# Return
-	
+
 
 # --------------------
 # Returns the indice of the left child in the data structure called "heap".
@@ -406,7 +436,7 @@ getLeftChildIndex:
 # --------------------
 getRightChildIndex:
 	sll		$v0,	$a0,	1	# 2 * index
-	addi	$v0,	$v0,	2	# (2 * i) + 2
+	addi	$v0,	$v0,	2	# (2 * index) + 2
 	jr		$ra					# Return index
 
 
@@ -437,29 +467,29 @@ swap:
 	sw		$s4,	0($sp)				# Store $s4
 
 	# Load array start
-	la		$s0,	arrayStart
-	lw		$s0,	0($s0)
-	
+	la		$s0,	arrayStart			# Load label of arrayStart
+	lw		$s0,	0($s0)				# Load address of arrayStart
+
 	# Get address and value of array[i]
 	sll		$s1,	$a0,		2		# i = i * 4
 	add		$s1,	$s0,		$s1		# Get address of index i in array
 	lw		$s2,	0($s1)				# s2 = array[i]
-	
+
 	# Get address and value of array[j]
 	sll		$s3,	$a1,		2		# j = j * 4
 	add		$s3,	$s0,		$s3		# Get address of index j in array
 	lw		$s4,	0($s3)				# $s4 = array[j]
-	
+
 	# Swap the values
 	sw		$s4,	0($s1)				# array[i] = array[j]
 	sw		$s2,	0($s3)				# array[i] = array[j]
-	
+
 	# Reload overriden registers
 	lw		$s0,	16($sp)				# Reload $s0
 	lw		$s1,	12($sp)				# Reload $s1
 	lw		$s2,	8($sp)				# Reload $s2
 	lw		$s3,	4($sp)				# Reload $s3
 	lw		$s4,	0($sp)				# Reload $s4
-	
+
 	addi	$sp,	$sp,	20			# Free allocated space
 	jr $ra								# Return
